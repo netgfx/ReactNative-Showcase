225790a81c2bef4cf6f84d384412cbe8cb6ad197
m.dobekidis <m.dobekidis@7linternational.com>
2019-10-04 13:14:25 +0300

    added flip animation

--- components/StickyScroll.js
+++ components/StickyScroll.js
@@ -2,24 +2,49 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
-import { Animated, FlatList, Dimensions } from 'react-native';
+import { Animated, FlatList, Dimensions, TouchableOpacity, Image } from 'react-native';
 const { height, width } = Dimensions.get('window');
 
 export default class ScrollSwagger extends Component {
 	constructor(props) {
 		super(props);
-
+		this.animatedValue = new Animated.Value(0);
+		this.value = 0;
+		this.animatedValue.addListener(({ value }) => {
+			this.value = value;
+			console.log(value);
+			if (this.state.flipped == false && value >= 90 && value < 180) {
+				this.setState({
+					img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png',
+					flipped: true
+				});
+			} else if (this.state.flipped == true && value <= 90 && value > 0) {
+				this.setState({
+					img: 'https://images.alphacoders.com/371/thumb-1920-371.jpg',
+					flipped: false
+				});
+			}
+		});
 		this.state = {
 			scrollY: new Animated.Value(0),
-			dataSource: [ { key: 'Sticky' }, { key: 'Dan' }, { key: 'Dominic' }, { key: 'Jackson' }, { key: 'James' }, { key: 'Joel' }, { key: 'John' }, { key: 'Jillian' }, { key: 'Jimmy' }, { key: 'Julie' }, { key: 'John' }, { key: 'Jillian' }, { key: 'Jimmy' }, { key: 'Julie' } ]
+			dataSource: [ { key: 'Sticky' }, { key: 'Dan' }, { key: 'Dominic' }, { key: 'Jackson' }, { key: 'James' }, { key: 'Joel' }, { key: 'John' }, { key: 'Jillian' }, { key: 'Jimmy' }, { key: 'Julie' }, { key: 'John' }, { key: 'Jillian' }, { key: 'Jimmy' }, { key: 'Julie' } ],
+			img: 'https://images.alphacoders.com/371/thumb-1920-371.jpg',
+			flipped: false
 		};
 	}
+
+	componentDidMount() {
+		Image.prefetch('https://images.alphacoders.com/371/thumb-1920-371.jpg');
+		Image.prefetch('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png');
+	}
+
 	renderRow(rowData) {
 		console.log(rowData.item.key);
 		let color = 'white';
 		if (rowData.item.key === 'Sticky') {
 			color = 'purple';
 		}
+
 		return (
 			<View
 				style={{
@@ -37,6 +62,11 @@ export default class ScrollSwagger extends Component {
 		);
 	}
 	render() {
+		this.SetInterpolate = this.animatedValue.interpolate({
+			inputRange: [ 0, 180 ],
+			outputRange: [ '180deg', '360deg' ]
+		});
+
 		var headMov = this.state.scrollY.interpolate({
 			inputRange: [ 0, 180, 181 ],
 			outputRange: [ 0, -180, -180 ]
@@ -65,6 +95,11 @@ export default class ScrollSwagger extends Component {
 			inputRange: [ 0, 100 ],
 			outputRange: [ '0deg', '360deg' ]
 		});
+
+		const Rotate_Y_AnimatedStyle = {
+			transform: [ { rotateY: this.SetInterpolate }, { translateY: misMovY } ]
+		};
+
 		return (
 			<View style={{ flex: 1 }}>
 				<FlatList
@@ -85,22 +120,25 @@ export default class ScrollSwagger extends Component {
 						}
 					)}
 					ListHeaderComponent={
-						<Animated.Image
-							source={{
-								uri: 'https://images.alphacoders.com/371/thumb-1920-371.jpg'
-							}}
-							style={{
-								bottom: 0,
-								width: width,
-								height: 250,
-								opacity: imgOp,
-								transform: [ { translateY: misMovY } ]
-							}}
-						/>
+						<TouchableOpacity onPress={() => this._flipImage()}>
+							<Animated.Image
+								source={{
+									uri: this.state.img
+								}}
+								style={[
+									{
+										bottom: 0,
+										width: width,
+										height: 250,
+										opacity: imgOp
+									},
+									Rotate_Y_AnimatedStyle
+								]}
+							/>
+						</TouchableOpacity>
 					}
-					//renderScrollComponent={this.renderScroll.bind(this)}
 				/>
-				<Animated.View
+				{/* <Animated.View
 					style={{
 						top: -height + 100,
 						zIndex: 2,
@@ -111,11 +149,40 @@ export default class ScrollSwagger extends Component {
 						alignSelf: 'center',
 						transform: [ { translateY: hamovY }, { translateX: hamovX }, { rotate: harot } ]
 					}}
-				/>
+				/> */}
 			</View>
 		);
 	}
 	_handleScroll(e) {
 		// console.log(e.nativeEvent.contentOffset.y, "jvjhvhm");
+	}
+
+	_flipImage() {
+		console.log('flipping!');
+		if (this.value >= 90) {
+			Animated.spring(this.animatedValue, {
+				toValue: 0,
+				tension: 10,
+				friction: 8,
+				useNativeDriver: true
+			}).start(() => {
+				console.log('end1');
+				this.setState({
+					flipped: true
+				});
+			});
+		} else {
+			Animated.spring(this.animatedValue, {
+				toValue: 180,
+				tension: 10,
+				friction: 8,
+				useNativeDriver: true
+			}).start(() => {
+				console.log('end2');
+				this.setState({
+					flipped: false
+				});
+			});
+		}
 	}
 }
